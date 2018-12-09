b0bcdfc1c67f8a90aa8643a9b484a036de436257
anirudhit <anirudhit@outlook.com>
2018-12-09 00:59:00 -0600

    interactive-dictionary
    - Added checks for the words
    - Optimised the code structure

--- interactive-dictionary/application.py
+++ interactive-dictionary/application.py
@@ -6,17 +6,28 @@ dictionary_data = json.load(open("data.json"))
 def search_word(word):
     word = word.lower()
     if word in dictionary_data:
-        for meaning in dictionary_data[word]:
-            print(meaning)
-    else:
-        similar_word = get_close_matches(word,dictionary_data.keys())
-        if len(similar_word):
-            print("Are you looking for the similar word " + "\""+ similar_word[0] + "\"")
-            search = input("To search type Yes or No to exit: ")
-            if search.lower() == "yes":
-                search_word(similar_word[0])
+        return dictionary_data[word]
+    elif word.title() in dictionary_data:
+        return dictionary_data[word.title()]
+    elif word.upper() in dictionary_data:
+        return dictionary_data[word.upper()]
+    elif len(get_close_matches(word,dictionary_data.keys(),cutoff=0.8)):
+        similar_word = get_close_matches(word,dictionary_data.keys(),cutoff=0.8)
+        print("Are you looking %s instead" % similar_word[0])
+        search_suggestion = input("Are you looking %s instead? Enter 'Y' if yes or 'N' if no: ")
+        if search_suggestion.lower() == "y":
+            return dictionary_data[similar_word[0]]
+        elif search_suggestion.lower() == "n":
+            return "The word doesn't exist. Please double check the word."
         else:
-            print("The word is not available. Please check the word")
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check the word."
 
 word = input("Enter a word to search: ")
-search_word(word)
+exp_output = search_word(word)
+if type(exp_output) == str:
+    print(exp_output)
+else:
+    for meaning in exp_output:
+        print(meaning)
