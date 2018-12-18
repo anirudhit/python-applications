54a6df6d99e61c827f98e30c83999e2d29b73c2a
anirudhit <anirudhit@outlook.com>
2018-12-18 14:32:53 -0600

    mapping
    - Added color_mapper method to add color to the marker

--- mapping/map1.py
+++ mapping/map1.py
@@ -7,6 +7,15 @@ lat=list(data["LAT"])
 lon=list(data["LON"])
 elev=list(data["ELEV"])
 
+def color_mapper(elevation):
+    if(elevation < 1000):
+        color='green'
+    elif(1000<= elevation < 3000):
+        color='orange'
+    else:
+        color='red'
+    return color
+
 html = """<h4>Volcano information:</h4>
 Height: %s m
 """
@@ -14,7 +23,8 @@ Height: %s m
 fg = folium.FeatureGroup(name="My map1")
 for lt, ln, el in zip(lat, lon, elev):
     iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt,ln],popup=folium.Popup(iframe), tooltip=folium.Tooltip(text=str(el) + " M"), icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt,ln],popup=folium.Popup(iframe), tooltip=folium.Tooltip(text=str(el) + " M"),
+    icon=folium.Icon(color=color_mapper(el))))
 map.add_child(fg)
 
 map.save("Map1.html")
